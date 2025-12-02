775ddd954e55f84890b91a8a1f4532ddd2169d4f
Arnab <arnabchoudhury2005@gmail.com>
2025-12-03 00:01:36 +0530

    Solving error in server

--- server/server.js
+++ server/server.js
@@ -9,45 +9,49 @@ import { Server } from "socket.io";
 
 //Create Express app and HTTP server
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 //Initialize socket.io server
 export const io = new Server(server, {
-    cors: {origin: "*"}
-})
+  cors: { origin: "*" },
+});
 
 //Store online users
 export const userSocketMap = {}; // { userId: socketId }
 
 //Socket.io connection handler
-io.on("connection", (socket)=>{
-    const userId = socket.handshake.query.userId;
-    console.log("User Connected", userId);
-    
-    if(userId) userSocketMap[userId] = socket.id;
-    
-    //Emit online users to all connected client
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
 
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected", userId);
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers", Object.keys(userSocketMap))
-    })
-})
+  if (userId) userSocketMap[userId] = socket.id;
+
+  //Emit online users to all connected client
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 //Middleware setup
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 //Routes setup
-app.use("/api/status", (req, res)=> res.send("Server is live"));
+app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
-app.use("/api/messages", messageRouter)
+app.use("/api/messages", messageRouter);
 
 //Connect to MongoDB
 await connectDB();
 
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
+//Export server for Versel
+export default server;
